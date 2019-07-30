47b8fb5741466ba03e6104117ba918e42d895ca8
lindbrook <lindbrook@gmail.com>
2019-07-30 10:40:22 -0700

    rename bioc() to bioc_download()

--- R/bioconductorDownloads.R
+++ R/bioconductorDownloads.R
@@ -30,14 +30,14 @@ bioconductorDownloads <- function(pkg = NULL, year = NULL, month = NULL,
 
   if (length(pkg) > 1) {
     dat <- lapply(pkg, function(p) {
-      bioc(p, year, month, end.year, end.month, observation, current.yr,
-        current.mo, cal.date)
+      bioc_download(p, year, month, end.year, end.month, observation,
+        current.yr, current.mo, cal.date)
     })
     names(dat) <- pkg
 
   } else if (length(pkg) == 1) {
-    dat <- bioc(pkg, year, month, end.year, end.month, observation, current.yr,
-      current.mo, cal.date)
+    dat <- bioc_download(pkg, year, month, end.year, end.month, observation,
+      current.yr, current.mo, cal.date)
   }
 
   out <- list(data = dat, pkg = pkg, obs = observation, date = cal.date,
@@ -46,7 +46,64 @@ bioconductorDownloads <- function(pkg = NULL, year = NULL, month = NULL,
   out
 }
 
-bioc <- function(pkg, year, month, end.year, end.month, observation,
+#' Plot method for bioconductor_downloads().
+#'
+#' @param x object.
+#' @param count Character. "download" or "ip".
+#' @param add.points Logical. Add points.
+#' @param smooth Logical. Add stats::lowess smoother.
+#' @param smooth.f Numeric. smoother span.
+#' @param ... Additional plotting parameters.
+#' @export
+#' @examples
+#' plot(bioconductorDownloads(pkg = "graph"))
+#' plot(bioconductorDownloads(pkg = "graph", year = 2019))
+#' plot(bioconductorDownloads(pkg = "graph", year = 2014, end.year = 2018, month = 6, end.month = 3))
+
+plot.bioconductor <- function(x, count = "download", add.points = TRUE,
+  smooth = FALSE, smooth.f = 2/3, ...) {
+
+  next.year <- as.Date(paste0(data.table::year(x$date) + 1, "-01-01"))
+  yr.in.progress <- ifelse(x$date < next.year, TRUE, FALSE)
+
+  if (is.data.frame(x$data)) {
+    bioc_plot(x$data, x, count, add.points, smooth, smooth.f, yr.in.progress)
+  } else if (is.list(x$data)) {
+    invisible(lapply(x$data, function(dat) {
+      bioc_plot(dat, x, count, add.points, smooth, smooth.f, yr.in.progress)
+    }))
+  }
+}
+
+#' Print method for bioconductorDownloads().
+#' @param x object.
+#' @param ... Additional parameters.
+#' @export
+
+print.bioconductor <- function(x, ...) {
+  if (is.data.frame(x$data)) print(x$data)
+  else if (is.list(x$data)) {
+    out <- do.call(rbind, x$data)
+    row.names(out) <- NULL
+    print(out)
+  }
+}
+
+#' Summary method for bioconductorDownloads().
+#' @param object Object.
+#' @param ... Additional parameters.
+#' @export
+
+summary.bioconductor <- function(object, ...) {
+  if (is.data.frame(object$data)) object$data
+  else if (is.list(object$data)) {
+    out <- do.call(rbind, object$data)
+    row.names(out) <- NULL
+    out
+  }
+}
+
+bioc_download <- function(pkg, year, month, end.year, end.month, observation,
   current.yr, current.mo, cal.date) {
 
   if (is.null(pkg)) {
@@ -173,68 +230,6 @@ bioc <- function(pkg, year, month, end.year, end.month, observation,
 
   dat
 }
-
-#' Plot method for bioconductor_downloads().
-#'
-#' @param x object.
-#' @param count Character. "download" or "ip".
-#' @param add.points Logical. Add points.
-#' @param smooth Logical. Add stats::lowess smoother.
-#' @param smooth.f Numeric. smoother span.
-#' @param ... Additional plotting parameters.
-#' @export
-#' @examples
-#' plot(bioconductorDownloads(pkg = "graph"))
-#' plot(bioconductorDownloads(pkg = "graph", year = 2019))
-#' plot(bioconductorDownloads(pkg = "graph", year = 2014, end.year = 2018, month = 6, end.month = 3))
-
-plot.bioconductor <- function(x, count = "download", add.points = TRUE,
-  smooth = FALSE, smooth.f = 2/3, ...) {
-
-  next.year <- as.Date(paste0(data.table::year(x$date) + 1, "-01-01"))
-  yr.in.progress <- ifelse(x$date < next.year, TRUE, FALSE)
-
-  if (is.data.frame(x$data)) {
-    bioc_plot(x$data, x, count, add.points, smooth, smooth.f, yr.in.progress)
-  } else if (is.list(x$data)) {
-    invisible(lapply(x$data, function(dat) {
-      bioc_plot(dat, x, count, add.points, smooth, smooth.f, yr.in.progress)
-    }))
-  }
-}
-
-#' Print method for bioconductorDownloads().
-#' @param x object.
-#' @param ... Additional parameters.
-#' @export
-
-print.bioconductor <- function(x, ...) {
-  if (is.data.frame(x$data)) print(x$data)
-  else if (is.list(x$data)) {
-    out <- do.call(rbind, x$data)
-    row.names(out) <- NULL
-    print(out)
-  }
-}
-
-#' Summary method for bioconductorDownloads().
-#' @param object Object.
-#' @param ... Additional parameters.
-#' @export
-
-summary.bioconductor <- function(object, ...) {
-  if (is.data.frame(object$data)) object$data
-  else if (is.list(object$data)) {
-    out <- do.call(rbind, object$data)
-    row.names(out) <- NULL
-    out
-  }
-}
-
-extractYearMonth <- function(z) substr(z, 1, 7)
-
-#' @param dat object.
-#' @noRd
 
 bioc_plot <- function(dat, x, count, add.points, smooth, smooth.f,
   yr.in.progress) {
