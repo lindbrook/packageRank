62b221da7bb810158e2601b5b2bd1858b6011874
lindbrook <lindbrook@gmail.com>
2019-09-20 15:40:44 -0700

    copyedit function documentation

--- R/cranDependenciesNetwork.R
+++ R/cranDependenciesNetwork.R
@@ -1,6 +1,6 @@
 #' Package dependencies graph.
 #'
-#' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores. See \code{vignette("Parallelization")} for details.
+#' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores.
 #' @note Uses 'igraph' package.
 #' @export
 
@@ -10,7 +10,6 @@ cranDependenciesNetwork <- function(multi.core = TRUE) {
     df <- expand.grid(x[1], x[-1], stringsAsFactors = FALSE)
     stats::setNames(df, c("node1", "node2"))
   }, mc.cores = cores)
-
   edge.list <- do.call(rbind, edges)
   igraph::graph_from_data_frame(edge.list, directed = TRUE)
 }
