296375fd3bb1558b3f2b223410982ebc376ba0af
lindbrook <lindbrook@gmail.com>
2019-09-24 17:54:52 -0700

    add cranPackages()

---
+++ R/cranPackages.R
@@ -0,0 +1,13 @@
+#' Current R packages (vector of package names).
+#'
+#' Uses tools::CRAN_package_db().
+#' @export
+
+cranPackages <- function() {
+  cran_db <- tools::CRAN_package_db()
+  priority.recommended <- vapply(cran_db$Depends, function(x) {
+    grepl("R \\(>= 3.7\\)", x)
+  }, logical(1L))
+  cran_db[-which(priority.recommended), "Package"]
+}
+
