fd4f911855fb446b0454f881f7d8b280395d0d30
lindbrook <lindbrook@gmail.com>
2019-11-07 09:45:59 -0800

    add packageLog(filter = FALSE)

--- R/packageLog.R
+++ R/packageLog.R
@@ -3,12 +3,13 @@
 #' From RStudio's CRAN Mirror http://cran-logs.rstudio.com/
 #' @param packages Character. Vector of package name(s).
 #' @param date Character. Date.
+#' @param filter Logical or Numeric. If Logical, 1000 bytes, If Numeric, set minimum package size in bytes.
 #' @param memoization Logical. Use memoization when downloading logs.
 #' @return An R data frame.
 #' @export
 
 packageLog <- function(packages = "HistData", date = Sys.Date() - 1,
-  memoization = TRUE) {
+  filter = FALSE, memoization = TRUE) {
     
   ymd <- as.Date(date)
   if (ymd > Sys.Date()) stop("Can't see into the future!")
@@ -23,8 +24,17 @@ packageLog <- function(packages = "HistData", date = Sys.Date() - 1,
     msg <- "Check your internet connection or try the previous day."
     stop("Log for ", date, " not (yet) available. ", msg)
   }
-
+ 
   cran_log <- as.data.frame(cran_log[!is.na(cran_log$package), ])
+   
+  if (filter) {
+    if (is.numeric(filter)) {
+      cran_log <- cran_log[cran_log$size >= filter, ]
+    } else if (is.logical(filter)) {
+      cran_log <- cran_log[cran_log$size >= 1000, ]
+    } else stop ("'filter' must be Logical or Numeric.")
+  }
+
   pkg_log <- cran_log[cran_log$package %in% packages, ]
   pkg_log[order(pkg_log$package), ]
 }
