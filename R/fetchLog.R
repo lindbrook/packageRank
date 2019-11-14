c9dd7c94dcfb07aee4514494c7d58eb11534b8c8
lindbrook <lindbrook@gmail.com>
2019-11-13 16:45:11 -0800

    output as data frame

--- R/fetchLog.R
+++ R/fetchLog.R
@@ -5,5 +5,5 @@
 #' @export
 #' @note mFetchLog() is memoized version.
 
-fetchLog <- function(x) data.table::fread(x)
+fetchLog <- function(x) as.data.frame(data.table::fread(x))
 mfetchLog <- memoise::memoise(fetchLog)
