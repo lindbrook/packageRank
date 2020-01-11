56fa2b28dc9fbbe0a17ddd61bf7134ddacb7a534
lindbrook <lindbrook@gmail.com>
2020-01-11 10:03:05 -0800

    amend function documentation

--- R/packageRank.R
+++ R/packageRank.R
@@ -2,8 +2,8 @@
 #'
 #' From RStudio's CRAN Mirror http://cran-logs.rstudio.com/
 #' @param packages Character. Vector of package name(s).
-#' @param date Character. Date. yyyy-mm-dd.
-#' @param size.filter Logical or Numeric. If Logical, TRUE size.filters out downloads less than 1000 bytes. If Numeric, a postive value (bytes) sets the minimum download size to consider; a negative value sets the maximum download size to consider.
+#' @param date Character. Date. "yyyy-mm-dd".
+#' @param size.filter Logical or Numeric. If Logical, TRUE filters out downloads less than 1000 bytes. If Numeric, a postive value sets the minimum download size (in bytes) to consider; a negative value sets the maximum download size to consider.
 #' @param version.filter Logical.
 #' @param memoization Logical. Use memoization when downloading logs.
 #' @return An R data frame.
