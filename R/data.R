30379088df257602ea5310877b61d92061f35d95
lindbrook <lindbrook@gmail.com>
2020-02-12 10:43:51 -0800

    add blog.post data

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' blog post data.
+#'
+#' \describe{
+#'    \item{\code{archive.pkg_ver}}{}
+#'    \item{\code{archive.pkg_ver.filtered}}{}
+#'    \item{\code{cran.pkg_ver}}{}
+#'    \item{\code{cran.pkg_ver.filtered}}{}
+#'    \item{\code{dl.ct}}{}
+#'    \item{\code{dl.ct2}}{}
+#'    \item{\code{pkg.ct}}{}
+#'    \item{\code{pkg.ct2}}{}
+#' }
+#' @format A list with 8 elements.
+#' @docType data
+"blog.data"
+
+# blog.data <- list(archive.pkg_ver = archive.pkg_ver,
+#                   archive.pkg_ver.filtered = archive.pkg_ver.filtered,
+#                   cran.pkg_ver = cran.pkg_ver,
+#                   cran.pkg_ver.filtered = cran.pkg_ver.filtered,
+#                   dl.ct = dl.ct,
+#                   dl.ct2 = dl.ct2,
+#                   pkg.ct = pkg.ct,
+#                   pkg.ct2 = pkg.ct2)
