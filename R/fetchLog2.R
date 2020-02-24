5b0aef661d50ab961efdc9d5a5ab8ae8020ba5c4
lindbrook <lindbrook@gmail.com>
2020-02-24 09:57:45 -0800

    add fetchLog2()

---
+++ R/fetchLog2.R
@@ -0,0 +1,21 @@
+#' Fetch Package Logs.
+#'
+#' @param date Character. Date. yyyy-mm-dd.
+#' @param memoization Logical. Use memoization when downloading logs.
+#' @export
+
+fetchLog2 <- function(date, memoization) {
+  ymd <- fixDate_2012(date)
+  if (ymd > Sys.Date()) stop("Can't see into the future!")
+  year <- as.POSIXlt(ymd)$year + 1900
+  rstudio.url <- "http://cran-logs.rstudio.com/"
+  url <- paste0(rstudio.url, year, '/', ymd, ".csv.gz")
+
+  if (RCurl::url.exists(url)) {
+    if (memoization) cran_log <- mfetchLog(url)
+    else cran_log <- fetchLog(url)
+  } else {
+    msg <- "Check your internet connection or try the previous day."
+    stop("Log for ", date, " not (yet) available. ", msg)
+  }
+}
