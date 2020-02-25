471044f6158b08c692091d890e26496169e4709a
lindbrook <lindbrook@gmail.com>
2020-02-25 09:46:37 -0800

    use fetchLog2() in packageLog() and packageRank()

--- R/packageLog.R
+++ R/packageLog.R
@@ -11,20 +11,7 @@
 packageLog <- function(packages = NULL, date = Sys.Date() - 1,
   filter = FALSE, memoization = TRUE) {
 
-  ymd <- fixDate_2012(date)
-  if (ymd > Sys.Date()) stop("Can't see into the future!")
-  year <- as.POSIXlt(ymd)$year + 1900
-  rstudio.url <- "http://cran-logs.rstudio.com/"
-  url <- paste0(rstudio.url, year, '/', ymd, ".csv.gz")
-
-  if (RCurl::url.exists(url)) {
-    if (memoization) cran_log <- mfetchLog(url)
-    else cran_log <- fetchLog(url)
-  } else {
-    msg <- "Check your internet connection or try the previous day."
-    stop("Log for ", date, " not (yet) available. ", msg)
-  }
-
+  cran_log <- fetchLog2(date = date, memoization = memoization)
   cran_log <- as.data.frame(cran_log[!is.na(cran_log$package), ])
 
   if (filter) {
