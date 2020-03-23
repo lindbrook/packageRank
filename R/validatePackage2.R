46fce584940a4c2990249e38172d24b15c61447d
lindbrook <lindbrook@gmail.com>
2020-03-23 16:41:52 -0700

    add validatePackage2() prototype

---
+++ R/validatePackage2.R
@@ -0,0 +1,15 @@
+#' Check for valid package names.
+#'
+#' @param packages Character. Vector of package name(s).
+#' @export
+
+validatePackage2 <- function(packages) {
+  check <- vapply(packages, function(x) {
+    class(try(pkgsearch::cran_package(x), silent = TRUE))
+  }, character(1L))
+
+  if (any(check == "try-error")) {
+    list(invalid = names(check[check == "try-error"]),
+         valid = names(check[check == "cran_package"]))
+  } else packages
+}
