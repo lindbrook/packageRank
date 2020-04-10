ae1f3fe93041be5afc7193871c2982ab51a03605
lindbrook <lindbrook@gmail.com>
2020-04-10 15:40:27 -0700

    add countsRanks() prototype

---
+++ R/countsRanks.R
@@ -0,0 +1,55 @@
+#' Counts v. Rank Percentiles.
+#'
+#' First week of March 2020.
+#' @param package Character.
+#' @param size.filter Logical.
+#' @export
+
+countsRanks <- function(package = "cholera", size.filter = FALSE) {
+  Downloads <- summary(cranDownloads(packages = package, from = "2020-03-01",
+    to = "2020-03-07"))$count
+  Date <- as.Date(c("2020-03-01", "2020-03-02", "2020-03-03", "2020-03-04",
+    "2020-03-05", "2020-03-06", "2020-03-07"))
+  percentiles <- vapply(Date, function(x) {
+    dat <- packageRank(packages = package, date = x, size.filter = size.filter)
+    summary(dat)$percentile
+  }, numeric(1L))
+  out <- list(package = package,
+              Downloads = Downloads,
+              Date = Date,
+              percentiles = percentiles)
+  class(out) <- "countsRanks"
+  out
+}
+
+#' Plot method for countsRanks().
+#'
+#' @param x object.
+#' @param ... Additional plotting parameters.
+#' @export
+
+plot.countsRanks <- function(x, ...) {
+  Downloads <- x$Downloads
+  Date <- x$Date
+  percentiles <- x$percentiles
+  par(mar = c(5, 4, 4, 4))
+  plot(Date, Downloads, type = "o", pch = 0, bty = "n", col = "gray")
+  par(new = TRUE)
+  plot(Date, percentiles, axes = FALSE, bty = "n", type = "o", pch = 16,
+    col= "red", xlab = NA, ylab = NA, lwd = 1.5)
+  axis(4, at = pretty(range(percentiles)), col.axis = "red", col = "red")
+  mtext("Rank Percentiles", side = 4, line = 3, col = "red")
+  abline(h = 50, lty = "dotted", col = "red")
+  title_a <- "Counts v. Rank Percentiles: '"
+  title_b <- "' March 1-7, 2020"
+  title(main = paste0(title_a, x$package, title_b))
+  legend(x = "topleft",
+         legend = c("Percentile", "Count"),
+         col = c("red", "gray"),
+         pch = c(15, 0),
+         bg = "white",
+         cex = 2/3,
+         lwd = 1,
+         title = NULL)
+  par(mar = c(5, 4, 4, 2))
+}
