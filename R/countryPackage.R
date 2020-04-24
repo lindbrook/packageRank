6e5b6333ea0da48d7826415b5c9877cf08d8d1ce
lindbrook <lindbrook@gmail.com>
2020-04-23 21:23:44 -0700

    use check10CharDate() and fixDate_2012()

--- R/countryPackage.R
+++ R/countryPackage.R
@@ -10,7 +10,9 @@
 countryPackage <- function(country = "US", date = Sys.Date() - 1,
   memoization = TRUE, sort = TRUE) {
 
-  cran_log <- fetchCranLog(date = date, memoization = memoization)
+  date <- check10CharDate(date)
+  ymd <- fixDate_2012(date)
+  cran_log <- fetchCranLog(date = ymd, memoization = memoization)
   sel <- !is.na(cran_log$package) | !is.na(cran_log$country)
   cran_log <- cran_log[sel, ]
   crosstab <- table(cran_log[cran_log$country == country, "package"])
