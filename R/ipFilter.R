fab055bbff3e9a2aec2f4b6ed8efae85a762f21d
lindbrook <lindbrook@gmail.com>
2020-07-24 09:24:48 -0700

    use number of unique packages

--- R/ipFilter.R
+++ R/ipFilter.R
@@ -2,11 +2,11 @@
 #'
 #' From RStudio's CRAN Mirror http://cran-logs.rstudio.com/
 #' @param date Character. Date. yyyy-mm-dd.
-#' @param cutpoint Numeric. Threshold number of downloads.
+#' @param cutpoint Numeric. Threshold number of unique packages downloaded.
 #' @param memoization Logical. Use memoization when downloading logs.
 #' @export
 
-ipFilter <- function(date = Sys.Date() - 1, cutpoint = 10000L,
+ipFilter <- function(date = Sys.Date() - 1, cutpoint = 15000L,
   memoization = TRUE) {
 
   date <- check10CharDate(date)
@@ -17,8 +17,11 @@ ipFilter <- function(date = Sys.Date() - 1, cutpoint = 10000L,
          !is.na(cran_log$size)
   cran_log <- cran_log[sel, ]
 
-  crosstab <- tapply(cran_log$package, cran_log$ip_id, length)
-  df <- data.frame(ip = names(crosstab), count = c(crosstab),
-    row.names = NULL)
-  as.numeric(unique(df[df$count >= cutpoint, "ip"]))
+  # number of unique packages downloaded by ip address.
+  crosstab <- tapply(cran_log$package, cran_log$ip_id, function(x) {
+    length(unique(x))
+  })
+
+  df <- data.frame(ip = names(crosstab), count = c(crosstab), row.names = NULL)
+  as.numeric(df[df$count >= cutpoint, "ip"])
 }
