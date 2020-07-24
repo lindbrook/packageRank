684795cfa7272a5513acc566ae507a6fce67bcbe
lindbrook <lindbrook@gmail.com>
2020-07-24 14:34:01 -0700

    add/use ipFilter() in packageLog() and cranDownloadsB()

--- R/cranDownloadsB.R
+++ R/cranDownloadsB.R
@@ -6,24 +6,26 @@
 #' @param check.package Logical. Validate and "spell check" package.
 #' @param small.filter Logical.
 #' @param triplet.filter Logical.
+#' @param ip.filter Logical.
 #' @param memoization Logical. Use memoization when downloading logs.
 #' @param dev.mode Logical. Use validatePackage0() to scrape CRAN.
 #' @export
 
 cranDownloadsB <- function(packages = "HistData", date = Sys.Date() - 1,
   check.package = TRUE, small.filter = TRUE, triplet.filter = TRUE,
-  memoization = TRUE, dev.mode = FALSE) {
+  ip.filter = TRUE, memoization = TRUE, dev.mode = FALSE) {
 
   if (check.package) packages <- checkPackage(packages, dev.mode)
   date <- check10CharDate(date)
   ymd <- fixDate_2012(date)
 
   cran_log <- packageLog(packages = packages, date = ymd, small.filter = FALSE,
-    triplet.filter = FALSE)
+    triplet.filter = FALSE, ip.filter = FALSE)
   ct <- vapply(cran_log, nrow, integer(1L))
 
   f.cran_log <- packageLog(packages = packages, date = ymd,
-    small.filter = small.filter, triplet.filter = triplet.filter)
+    small.filter = small.filter, triplet.filter = triplet.filter,
+    ip.filter = ip.filter)
   f.ct <- vapply(f.cran_log, nrow, integer(1L))
 
   data.frame(date = date, package = packages, downloads = ct,
