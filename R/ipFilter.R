4b25b8634de1104702dff523b99052e0c3fcde06
lindbrook <lindbrook@gmail.com>
2020-08-07 13:38:28 -0700

    add/set ipFilter3(floor = "auto")

--- R/ipFilter.R
+++ R/ipFilter.R
@@ -83,35 +83,56 @@ ipFilter2 <- function(date = Sys.Date() - 1, output = "ip", centers = 2L,
 #'
 #' From RStudio's CRAN Mirror http://cran-logs.rstudio.com/
 #' @param cran_log Object. cran log.
+#' @param floor Character or Numeric. "auto" or floor of number of unique packages downloaded by an IP address.
 #' @param output Character. "ip" vector of ip address; "df" data frame.
 #' @param centers Numeric. Number of k's for k-means clustering.
 #' @param nstart Numeric. Number of random sets.
 #' @export
 
-ipFilter3 <- function(cran_log, output = "ip", centers = 2L, nstart = 25L) {
+ipFilter3 <- function(cran_log, floor = "auto", output = "ip", centers = 2L,
+  nstart = 25L) {
+
+  if (!output %in% c("df", "ip")) stop('"output" must be "ip" or "df".')
+
   crosstab <- tapply(cran_log$package, cran_log$ip_id, function(x) {
     length(unique(x))
   })
 
-  df <- data.frame(ip = names(crosstab), count = c(crosstab), row.names = NULL)
-  df <- df[!duplicated(df$count), ]
-  km <- stats::kmeans(stats::dist(df$count), centers = centers, nstart = nstart)
+  if (floor == "auto") {
+    df <- data.frame(ip = names(crosstab), count = c(crosstab),
+      row.names = NULL)
+    df <- df[!duplicated(df$count), ]
+    km <- stats::kmeans(stats::dist(df$count), centers = centers,
+      nstart = nstart)
 
-  out <- data.frame(ip = df$ip, downloads = df$count, group = km$cluster)
-  ip.country <- cran_log[!duplicated(cran_log$ip), c("ip_id", "country")]
-  out <- merge(out, ip.country, by.x = "ip", by.y = "ip_id")
-  out <- out[, c("ip", "country", "downloads", "group")]
+    out <- data.frame(ip = df$ip, downloads = df$count, group = km$cluster)
+    ip.country <- cran_log[!duplicated(cran_log$ip), c("ip_id", "country")]
+    out <- merge(out, ip.country, by.x = "ip", by.y = "ip_id")
+    out <- out[, c("ip", "country", "downloads", "group")]
 
-  if (length(unique(km$cluster)) == 1) {
-    stop("No IP outliers!")
-  } else if (length(unique(km$cluster)) > 1) {
-    if (output == "ip") {
-      grp <- as.numeric(names(which.min(table(out$group))))
-      as.numeric(out[out$group == grp, "ip"])
-    } else if (output == "df") {
-      out[order(out$downloads, decreasing = TRUE), ]
+    if (length(unique(km$cluster)) == 1) {
+      stop("No IP outliers!")
+    } else if (length(unique(km$cluster)) > 1) {
+      if (output == "ip") {
+        grp <- as.numeric(names(which.min(table(out$group))))
+        out <- as.numeric(out[out$group == grp, "ip"])
+      } else if (output == "df") {
+        out <- out[order(out$downloads, decreasing = TRUE), ]
+      }
+    }
+  } else if (is.numeric(floor)) {
+    if (floor < 0) {
+      stop('"floor" must be >= 0.')
     } else {
-      stop('"output" must be "ip" or "df".')
+      if (output == "ip") {
+        out <- as.numeric(names(crosstab[crosstab >= floor]))
+      } else if (output == "df") {
+        df <- data.frame(ip = names(crosstab), count = c(crosstab),
+          row.names = NULL)
+        df$group <- ifelse(df$count >= floor, 1, 2)
+        out <- df[order(df$count, decreasing = TRUE), ]
+      }
     }
   }
+  out
 }
