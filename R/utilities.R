454388c87b5578eb94d806cddfe1cf349f0fb527
lindbrook <lindbrook@gmail.com>
2020-09-03 15:15:43 -0700

    rearrange arguments and add clean.output argument

--- R/utilities.R
+++ R/utilities.R
@@ -1,34 +1,37 @@
 #' Extract Package Logs.
 #'
+#' @param dat Object. List of logs.
 #' @param i Numeric. Day/ID.
 #' @param pkg Character.
-#' @param dat Object. List of logs.
+#' @param clean.output Logical.
 #' @export
 
-pkgLog0 <- function(i = 1, pkg = "cholera", dat) {
+pkgLog0 <- function(dat, i = 1, pkg = "cholera", clean.output = TRUE) {
   cran_log <- dat[[i]]
   tmp <- cran_log[!is.na(cran_log$package) & cran_log$package == pkg, ]
   tmp$t2 <- as.POSIXlt(paste(tmp$date, tmp$time), tz = "Europe/Vienna")
-  tmp[order(tmp$t2), c(1:6, 8:10)]
+  tmp <- tmp[order(tmp$t2), c(1:6, 8:10)]
+  if (clean.output) row.names(tmp) <- NULL
+  tmp
 }
-
 
 #' Extract Package Logs.
 #'
+#' @param dat Object. List of logs.
 #' @param i Numeric. Day/ID.
 #' @param triplet.filter Logical.
 #' @param ip.filter Logical.
-#' @param use.campaigns Logical.
+#' @param campaigns Logical. For use with ip.filter.
 #' @param small.filter Logical.
 #' @param sequence.filter Logical.
 #' @param pkg Character.
-#' @param dat Object. List of logs.
 #' @param multi.core Logical or Numeric.
+#' @param clean.output Logical.
 #' @export
 
-pkgLog <- function(i = 1, triplet.filter = TRUE, ip.filter = TRUE,
-  use.campaigns = TRUE, small.filter = TRUE, sequence.filter = TRUE,
-  pkg = "cholera", dat, multi.core = TRUE) {
+pkgLog <- function(dat, i = 1, triplet.filter = TRUE, ip.filter = TRUE,
+  campaigns = TRUE, small.filter = TRUE, sequence.filter = TRUE,
+  pkg = "cholera", multi.core = TRUE, clean.output = TRUE) {
 
   cores <- multiCore(multi.core)
   cran_log <- dat[[i]]
@@ -38,7 +41,7 @@ pkgLog <- function(i = 1, triplet.filter = TRUE, ip.filter = TRUE,
 
   if (ip.filter) {
     ip.outliers <- ipFilter3(cran_log)
-    if (use.campaigns) {
+    if (campaigns) {
       row.delete <- unlist(parallel::mclapply(ip.outliers, function(x) {
         campaigns(x, cran_log)
       }, mc.cores = cores))
@@ -56,5 +59,7 @@ pkgLog <- function(i = 1, triplet.filter = TRUE, ip.filter = TRUE,
   if (sequence.filter) tmp <- sequenceFilter(tmp)
 
   tmp$t2 <- as.POSIXlt(paste(tmp$date, tmp$time), tz = "Europe/Vienna")
-  tmp[order(tmp$t2), !names(tmp) %in% "t2"]
+  tmp <- tmp[order(tmp$t2), !names(tmp) %in% "t2"]
+  if (clean.output) row.names(tmp) <- NULL
+  tmp
 }
