66483342ced64ef49fc9876998a224fb617567bb
lindbrook <lindbrook@gmail.com>
2020-09-08 10:42:19 -0700

    add sequence.filter to packageCountry()

--- R/packageCountry.R
+++ R/packageCountry.R
@@ -3,20 +3,21 @@
 #' From RStudio's CRAN Mirror http://cran-logs.rstudio.com/
 #' @param packages Character. Vector of package name(s).
 #' @param date Character. Date. yyyy-mm-dd.
-#' @param memoization Logical. Use memoization when downloading logs.
 #' @param sort Logical. Sort by download count.
 #' @param na.rm Logical. Remove NAs.
 #' @param triplet.filter Logical.
 #' @param ip.filter Logical.
 #' @param small.filter Logical.
+#' @param sequence.filter Logical.
+#' @param memoization Logical. Use memoization when downloading logs.
 #' @param check.package Logical. Validate and "spell check" package.
 #' @param dev.mode Logical. Use validatePackage0() to scrape CRAN.
 #' @export
 
 packageCountry <- function(packages = NULL, date = Sys.Date() - 1,
   memoization = TRUE, sort = TRUE, na.rm = FALSE, triplet.filter = TRUE,
-  ip.filter = TRUE, small.filter = TRUE, check.package = TRUE,
-  dev.mode = FALSE) {
+  ip.filter = TRUE, small.filter = TRUE, sequence.filter = TRUE,
+  check.package = TRUE, dev.mode = FALSE) {
 
   if (!is.null(packages)) {
     if (check.package) {
@@ -25,8 +26,9 @@ packageCountry <- function(packages = NULL, date = Sys.Date() - 1,
   }
 
   p.log <- packageLog(packages = packages, date = date,
-    memoization = memoization, triplet.filter = triplet.filter,
-    ip.filter = ip.filter, small.filter = small.filter)
+    triplet.filter = triplet.filter, ip.filter = ip.filter,
+    small.filter = small.filter, sequence.filter = sequence.filter,
+    memoization = memoization)
 
   if (na.rm) {
     if (is.data.frame(p.log)) {
