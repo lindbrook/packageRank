f48de83f4d0700d4ea7faf79bc4ecb1fb3bca420
lindbrook <lindbrook@gmail.com>
2020-09-12 17:03:08 -0700

    fix typo

--- R/fetchLog.R
+++ R/fetchLog.R
@@ -4,7 +4,7 @@
 #' @import data.table memoise
 #' @importFrom R.utils decompressFile
 #' @export
-#' @note mFetchLog() is memoized version.
+#' @note mfetchLog() is memoized version.
 
 fetchLog <- function(x) as.data.frame(data.table::fread(x))
 mfetchLog <- memoise::memoise(fetchLog)
