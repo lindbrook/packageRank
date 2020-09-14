1afb645f37029ecaea566c593de4491291fa4167
lindbrook <lindbrook@gmail.com>
2020-09-14 14:01:47 -0700

    remove packages = NULL and dev.mode

--- R/packageCountry.R
+++ R/packageCountry.R
@@ -11,24 +11,17 @@
 #' @param sequence.filter Logical.
 #' @param memoization Logical. Use memoization when downloading logs.
 #' @param check.package Logical. Validate and "spell check" package.
-#' @param dev.mode Logical. Use validatePackage0() to scrape CRAN.
 #' @export
 
-packageCountry <- function(packages = NULL, date = Sys.Date() - 1,
+packageCountry <- function(packages = "cholera", date = Sys.Date() - 1,
   memoization = TRUE, sort = TRUE, na.rm = FALSE, triplet.filter = TRUE,
   ip.filter = TRUE, small.filter = TRUE, sequence.filter = TRUE,
-  check.package = TRUE, dev.mode = FALSE) {
-
-  if (!is.null(packages)) {
-    if (check.package) {
-      packages <- checkPackage(packages, dev.mode)
-    }
-  }
+  check.package = TRUE) {
 
   p.log <- packageLog(packages = packages, date = date,
     triplet.filter = triplet.filter, ip.filter = ip.filter,
     small.filter = small.filter, sequence.filter = sequence.filter,
-    memoization = memoization)
+    memoization = memoization, check.package = check.package)
 
   if (na.rm) {
     if (is.data.frame(p.log)) {
