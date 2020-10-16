8c41cdeb71a0386d4054451180e39a5bb97caca0
lindbrook <lindbrook@gmail.com>
2020-10-15 17:03:06 -0700

    add base R code to fetchCranLog()

--- R/fetchLog.R
+++ R/fetchLog.R
@@ -2,17 +2,25 @@
 #'
 #' @param date Character. Date. yyyy-mm-dd.
 #' @param memoization Logical. Use memoization when downloading logs.
+#' @param dev.mode Logical. Use Base R code.
 #' @export
 
-fetchCranLog <- function(date, memoization) {
+fetchCranLog <- function(date, memoization = FALSE, dev.mode = FALSE) {
   if (date > Sys.Date()) stop("Can't see into the future!")
   year <- as.POSIXlt(date)$year + 1900
   rstudio.url <- "http://cran-logs.rstudio.com/"
-  url <- paste0(rstudio.url, year, '/', date, ".csv.gz")
+  log.url <- paste0(rstudio.url, year, '/', date, ".csv.gz")
 
-  if (RCurl::url.exists(url)) {
-    if (memoization) cran_log <- mfetchLog(url)
-    else cran_log <- fetchLog(url)
+  if (RCurl::url.exists(log.url)) {
+    if (dev.mode) {
+      cran_log <- mfetchLogBase(log.url)
+    } else {
+      if (memoization) {
+        cran_log <- mfetchLog(log.url)
+      } else {
+        cran_log <- fetchLog(log.url)
+      }
+    }
   } else {
     msg <- "Check your internet connection or try the previous day."
     stop("Log for ", date, " not (yet) available. ", msg)
@@ -30,3 +38,17 @@ fetchCranLog <- function(date, memoization) {
 
 fetchLog <- function(x) as.data.frame(data.table::fread(x))
 mfetchLog <- memoise::memoise(fetchLog)
+
+#' Get gzipped data at URL.
+#'
+#' @param x Character. URL.
+#' @export
+#' @note mfetchLogBase() is memoized version.
+
+fetchLogBase <- function(x) {
+  connection <- gzcon(url(x))
+  dat <- readLines(connection)
+  cran_log <- read.csv(textConnection(dat))
+}
+
+mfetchLogBase <- memoise::memoise(fetchLogBase)
