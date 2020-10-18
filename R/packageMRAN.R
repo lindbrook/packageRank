a4a99259c5dc50c61eda13be90ed6ec6ce61bde0
lindbrook <lindbrook@gmail.com>
2020-10-18 11:30:47 -0700

    add package argument to packageMRAN()

--- R/packageMRAN.R
+++ R/packageMRAN.R
@@ -1,13 +1,19 @@
 #' Extract package data from MRAN (protoype).
 #'
 #' Binary or source size.
+#' @param package Character. Package name.
 #' @param date Character.
 #' @param type Character. "source", "mac", or "win".
+#' @param check.package Logical. Validate and "spell check" package.
 #' @export
 
-packageMRAN <- function(date = Sys.Date() - 1, type = "source") {
+packageMRAN <- function(package = "cholera", date = Sys.Date() - 1,
+  type = "source", check.package = TRUE) {
+
+  if (check.package) package <- checkPackage(package)
   ymd <- check10CharDate(date, repository = "MRAN")
-  root.url <- paste0("https://cran.microsoft.com/snapshot/", ymd)
+  mran.url <- "https://cran.microsoft.com/snapshot/"
+  root.url <- paste0(mran.url, ymd)
 
   if (type == "source") {
     url.suffix <- "/src/contrib"
@@ -21,18 +27,38 @@ packageMRAN <- function(date = Sys.Date() - 1, type = "source") {
   } else stop('type must be "source", "mac", or "win".')
 
   url <- paste0(root.url, url.suffix)
-  web_page <- mreadLines(url)
+  web_page <- mreadLines(url) # packageHistory.R
   pkg.sel <- grepl(file.extension, web_page, fixed = TRUE)
   pkg.data <- gsub("<.*?>", "", web_page[pkg.sel])
   pkg.data <- gsub("\\s+", " ", pkg.data)
   pkg.data <- strsplit(pkg.data, " ")
+  pkg.match <- grepl(package, pkg.data, fixed = TRUE)
 
-  out <- lapply(pkg.data, function(x) {
-    tmp <- unlist(strsplit(x[1], file.extension))
-    ptA <- unlist(strsplit(tmp, "_"))
-    data.frame(package = ptA[1], version = ptA[2], date = x[2], time = x[3],
-      size = x[4], snapshot = date, type = type)
-  })
+  if (sum(pkg.match) == 1) {
+    pkg.data <- unlist(pkg.data[pkg.match])
+  } else if (sum(pkg.match) > 1) {
+    pkg.data <- pkg.data[pkg.match]
+    pkg.nms <- vapply(pkg.data, function(x) {
+      tmp <- unlist(strsplit(x[1], file.extension))
+       unlist(strsplit(tmp, "_"))[1]
+    }, character(1L))
+    pkg.data <- unlist(pkg.data[pkg.nms == package])
+  } else if (sum(pkg.match) == 0) {
+    stop('Not on MRAN (on date). Check packageHistory(package) and add a day or two.')
+  }
 
-  do.call(rbind, out)
+  tmp <- unlist(strsplit(pkg.data[1], file.extension))
+  nm.ver <- unlist(strsplit(tmp, "_"))
+
+  mo.id <- vapply(month.abb, function(m) grepl(m, pkg.data[2]), logical(1L))
+  mo <- which(mo.id)
+  mo <- ifelse(mo < 10, paste0("0", mo), mo)
+
+  dt <- unlist(strsplit(pkg.data[2], "-"))
+  dt[2] <- mo
+  dt <- paste(rev(dt), collapse = "-")
+  dt <- as.POSIXlt(dt, tz = "Europe/Vienna")
+
+  data.frame(package = nm.ver[1], version = nm.ver[2], size = pkg.data[4],
+    timestamp = dt, snapshot = as.Date(date), type = type)
 }
