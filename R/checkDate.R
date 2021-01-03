4e4402b3fc6d2bfdeac36846582223e9cfdaf603
lindbrook <lindbrook@gmail.com>
2021-01-03 14:53:07 -0800

    revise checkDate() prototype

--- R/checkDate.R
+++ R/checkDate.R
@@ -1,11 +1,14 @@
 #' Check and validate "yyyy-mm-dd" date.
 #'
-#' @param date Character. \code{"yyyy-mm-dd"}.
+#' @param date Character. Date of desired log \code{"yyyy-mm-dd"}.
 #' @param repository Character. "CRAN" or "MRAN".
 #' @param upload.time Character. UTC upload time for logs "hh:mm" or "hh:mm:dd".
+#' @param tz Character. Local time zone. See OlsonNames().
 #' @export
 
-checkDate <- function(date, repository = "CRAN", upload.time = "17:00:00") {
+checkDate <- function(date, repository = "CRAN", upload.time = "17:00",
+  tz = Sys.timezone()) {
+
   date <- as.Date(date, optional = TRUE)
 
   if (is.na(date)) {
@@ -28,41 +31,94 @@ checkDate <- function(date, repository = "CRAN", upload.time = "17:00:00") {
     }
   }
 
-  delta.days <- difftime(date, logDate())
+  available.date <- logDate()
+  local.datetime <- dateTime(date, format(Sys.time(), "%H:%M:%S"), tz = tz)
+  local.utc <- as.POSIXlt(as.numeric(local.datetime),
+    origin = "1970-01-01", tz = "GMT")
+  local.date <- as.Date(format(local.utc, "%Y-%m-%d"))
 
-  if (delta.days > 0) {
-    local.time <- Sys.time()
-    nominal.time <- dateTime(date, format(local.time, "%H:%M:%S"),
-      tz = Sys.timezone())
-    nominal.utc <- as.POSIXlt(as.numeric(local.time), origin = "1970-01-01",
-      tz = "GMT")
+  yesterday.utc <- dateTime(Sys.Date() - 1, upload.time)
+  today.utc <- dateTime(Sys.Date(), upload.time)
+  tomorrow.utc <- dateTime(Sys.Date() + 1, upload.time)
+  day_after.utc <- dateTime(Sys.Date() + 2, upload.time)
 
-    # Info for "yesterday"'s log
-    east.of.dateline <- as.POSIXlt(as.numeric(nominal.time),
-      origin = "1970-01-01", tz = "Etc/GMT+12")
-    intl.dateline.date <- as.Date(format(east.of.dateline, "%Y-%m-%d"))
-    upload.utc <- dateTime(intl.dateline.date, upload.time)
+  if (local.utc < yesterday.utc) {
+    date
 
-    local.upload <- as.POSIXlt(as.numeric(upload.utc), origin = "1970-01-01",
-      tz = Sys.timezone())
-    local.upload <- format(local.upload, format = "%Y-%m-%d %H:%M:%S %Z")
+  } else if (local.utc >= yesterday.utc & local.utc < today.utc) {
+    if (local.date > available.date) {
+      delta <- -difftime(local.utc, today.utc)
 
-    effective.date <- intl.dateline.date - 1
-    delta <- difftime(nominal.utc, upload.utc)
-
-    if (delta < 0) {
-      Time <- -round(unclass(delta)[1])
+      Time <- unclass(delta)[1]
       Unit <- attributes(delta)$units
-      if (Unit == "hours") {
-        Unit <- ifelse(Time == 1, substr(Unit, 1, nchar(Unit) - 1), Unit)
-        msg <- paste0("Log for ", effective.date, " in available in ",
-          paste(Time, Unit), " at ", local.upload, ".")
-      } else {
-        Unit <- ifelse(Time == 1, substr(Unit, 1, nchar(Unit) - 1), Unit)
-        msg <- paste0(effective.date, " in future! Log available in ",
-          paste(Time, Unit), ".")
+
+      if (Unit %in% c("secs", "mins")) {
+        Time <- round(Time)
+      } else if (Unit == "hours") {
+        Time <- round(Time, 1)
       }
-      stop(msg, call. = FALSE)
-    } else effective.date
-  } else date
+
+      Unit <- ifelse(Time == 1, substr(Unit, 1, nchar(Unit) - 1), Unit)
+
+      local.upload <- as.POSIXlt(as.numeric(yesterday.utc),
+        origin = "1970-01-01", tz = tz)
+      local.upload <- format(local.upload, format = "%H:%M %Z")
+
+      msg <- paste0(date, " log should be available in ", paste(Time, Unit),
+        " at ", local.upload, ". Using previous log.")
+      warning(msg, call. = FALSE)
+      available.date
+    } else if (local.date <= available.date) {
+      available.date
+    }
+
+  } else if (local.utc >= today.utc & local.utc < tomorrow.utc) {
+    delta <- -difftime(local.utc, tomorrow.utc)
+
+    Time <- unclass(delta)[1]
+    Unit <- attributes(delta)$units
+
+    if (Unit %in% c("secs", "mins")) {
+      Time <- round(Time)
+    } else if (Unit == "hours") {
+      Time <- round(Time, 1)
+    }
+
+    Unit <- ifelse(Time == 1, substr(Unit, 1, nchar(Unit) - 1), Unit)
+
+    local.upload <- as.POSIXlt(as.numeric(yesterday.utc),
+      origin = "1970-01-01", tz = tz)
+    local.upload <- format(local.upload, format = "%H:%M %Z")
+
+    msg <- paste0(date, " log should be available in ", paste(Time, Unit),
+      " at ", local.upload, ". Using previous log.")
+    warning(msg, call. = FALSE)
+    available.date
+
+  } else if (local.utc >= tomorrow.utc & local.utc < day_after.utc) {
+    delta <- difftime(local.utc, day_after.utc)
+
+    Time <- unclass(delta)[1]
+    Unit <- attributes(delta)$units
+
+    if (Unit %in% c("secs", "mins", "days")) {
+      Time <- round(Time)
+    } else if (Unit == "hours") {
+      Time <- round(Time, 1)
+    }
+
+    Unit <- ifelse(Time == 1, substr(Unit, 1, nchar(Unit) - 1), Unit)
+
+    local.upload <- as.POSIXlt(as.numeric(tomorrow.utc), origin = "1970-01-01",
+      tz = tz)
+    local.upload <- format(local.upload, format = "%d %b %H:%M %Z")
+
+    msg <- paste0("Date in future! ", date, " log should be available ",
+      local.upload, ".")
+    stop(msg, call. = FALSE)
+
+  } else if (local.utc >= day_after.utc) {
+     msg <- paste0("Date in future! ")
+     stop(msg, call. = FALSE)
+  }
 }
