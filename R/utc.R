a143150ecb8318e3d7f9b368682c69ed8c87258b
lindbrook <lindbrook@gmail.com>
2021-01-03 14:54:33 -0800

    add utc() and currentTime()

---
+++ R/utc.R
@@ -0,0 +1,27 @@
+#' Compute Coordinated Universal Time (UTC/GMT) for your local time.
+#'
+#' @param local.time Logical. Use current local time.
+#' @param date Character. Date "yyyy-mm-dd".
+#' @param time Character. Local time ime "hh:mm" or "hh:mm:dd".
+#' @param tz Character. Local time zone. See OlsonNames().
+#' @export
+
+utc <- function(local.time = TRUE, date = "2020-01-01", time = "12:00:00",
+  tz = "Europe/Vienna") {
+  if (local.time) {
+    x <- Sys.time()
+  } else {
+    x <- dateTime(date = date, time = time, tz = tz)
+  }
+  as.POSIXlt(as.numeric(x), origin = "1970-01-01", tz = "GMT")
+}
+
+#' Compute Current Local Time in Selected Time Zone.
+#'
+#' @param tz Character. Local time zone. See OlsonNames().
+#' @export
+
+currentTime <- function(tz = "Australia/Sydney") {
+  x <- Sys.time()
+  as.POSIXlt(as.numeric(x), origin = "1970-01-01", tz = tz)
+}
