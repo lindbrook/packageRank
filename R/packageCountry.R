cc2a667b398ce3984244f2dee2fa92d061c038ef
lindbrook <lindbrook@gmail.com>
2021-01-05 09:17:18 -0800

    set warning.msg = FALSE

--- R/packageCountry.R
+++ R/packageCountry.R
@@ -18,8 +18,8 @@ packageCountry <- function(packages = "cholera", date = NULL,
   ip.filter = TRUE, small.filter = TRUE, sequence.filter = TRUE,
   check.package = TRUE) {
 
-  if (is.null(date)) ymd <- logDate()
-  else ymd <- checkDate(date)
+  if (is.null(date)) ymd <- logDate(warning.msg = FALSE)
+  else ymd <- checkDate(date, warning.msg = FALSE)
   ymd <- fixDate_2012(ymd)
 
   p.log <- packageLog(packages = packages, date = ymd,
