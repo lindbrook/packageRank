1be6d5a11e1400048b84a7296cd0b343bcd4ceaf
lindbrook <lindbrook@gmail.com>
2021-01-13 20:01:18 -0800

    replace as.POSIXlt() with as.POSIXct()

--- R/dateTime.R
+++ R/dateTime.R
@@ -2,10 +2,10 @@
 #'
 #' Time via POSIXlt date.
 #' @param date Character. "yyyy-mm-dd".
-#' @param time Character. "hh:mmm:ss".
+#' @param time Character. "hh:mmm" or "hh:mmm:ss".
 #' @param tz Character. Time zone.
 #' @export
 
-dateTime <- function(date = "2020-08-14", time = "09:00:00", tz = "GMT") {
- as.POSIXlt(paste(date, time), tz = tz)
+dateTime <- function(date = "2021-01-01", time = "09:00", tz = "GMT") {
+  as.POSIXct(paste(date, time), tz = tz)
 }
