c927260ef06a77bdf5f7be3f34e483fb0ba55f65
lindbrook <lindbrook@gmail.com>
2021-01-27 13:46:24 -0800

    add/amend utc(), utc0(), currentTime(), and logPost()

--- R/utc.R
+++ R/utc.R
@@ -1,27 +1,39 @@
-#' Compute Coordinated Universal Time (UTC/GMT) for your local time.
+#' Compute Coordinated Universal Time (UTC/GMT) for Your Local Time.
 #'
-#' @param local.time Logical. Use current local time.
-#' @param date Character. Date "yyyy-mm-dd".
-#' @param time Character. Local time ime "hh:mm" or "hh:mm:ss".
-#' @param tz Character. Local time zone. See OlsonNames().
 #' @export
 
-utc <- function(local.time = TRUE, date = "2020-01-01", time = "12:00:00",
-  tz = "Europe/Vienna") {
-  if (local.time) {
-    x <- Sys.time()
-  } else {
-    x <- dateTime(date = date, time = time, tz = tz)
-  }
+utc <- function() {
+  as.POSIXlt(as.numeric(Sys.time()), origin = "1970-01-01", tz = "GMT")
+}
+
+#' Compute Coordinated Universal Time (UTC/GMT) for Specified Local Time.
+#'
+#' @param date Character. Date "yyyy-mm-dd".
+#' @param time Character. Local time "hh:mm" or "hh:mm:ss".
+#' @param tz Character. Local time zone. Use OlsonNames() or Sys.timezone().
+#' @export
+
+utc0 <- function(date = "2020-01-01", time = "12:00:00", tz = "Europe/Vienna") {
+  x <- dateTime(date = date, time = time, tz = tz)
   as.POSIXlt(as.numeric(x), origin = "1970-01-01", tz = "GMT")
 }
 
 #' Compute Current Local Time in Selected Time Zone.
 #'
-#' @param tz Character. Local time zone. See OlsonNames().
+#' @param tz Character. Local time zone. Use OlsonNames() or Sys.timezone().
 #' @export
 
 currentTime <- function(tz = "Australia/Sydney") {
-  x <- Sys.time()
-  as.POSIXct(as.numeric(x), origin = "1970-01-01", tz = tz)
+  as.POSIXct(as.numeric(Sys.time()), origin = "1970-01-01", tz = tz)
+}
+
+#' Compute Date of log, GMT and Local Posing Time.
+#'
+#' @param tz Character. Local time zone. Use OlsonNames() or Sys.timezone().
+#' @export
+
+logPostInfo <- function(tz = Sys.timezone()) {
+  x <- dateTime(Sys.Date(), time = "17:00", tz = "GMT")
+  local <- as.POSIXlt(as.numeric(x), origin = "1970-01-01", tz = tz)
+  list(log.date = as.Date(format(x, "%Y-%m-%d")) - 1, GMT = x, local = local)
 }
