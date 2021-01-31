dbdd2595c4d96fc17671223a6ab01f094ea08db4
lindbrook <lindbrook@gmail.com>
2021-01-31 10:34:12 -0800

    add sizeFilter()

---
+++ R/sizeFilter.R
@@ -0,0 +1,19 @@
+#' Filter out size anomalies (prototype).
+#'
+#' Logs from RStudio's CRAN Mirror http://cran-logs.rstudio.com/
+#' @param dat Object. Package log entries.
+#' @param packages Character. Vector of package name(s).
+#' @export
+
+sizeFilter <- function(dat, packages) {
+  size.data <- lapply(packages, packageCRAN2)
+
+  lapply(seq_along(dat), function(i) {
+    sz <- size.data[[i]]
+    tmp <- dat[[i]]
+    leftover <- tmp[tmp$version != unique(sz$version), ]
+    tmp <- tmp[tmp$version == unique(sz$version), ]
+    tmp <- tmp[tmp$size >= min(sz$bytes), ]
+    rbind(tmp, leftover)
+  })
+}
