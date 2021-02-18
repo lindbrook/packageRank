8dd4660786ce22a2162bc175e662f8b8559ac9d3
lindbrook <lindbrook@gmail.com>
2021-02-18 09:54:45 -0800

    add localTime()

--- R/utc.R
+++ R/utc.R
@@ -31,6 +31,22 @@ currentTime <- function(tz = "Australia/Sydney") {
   as.POSIXct(as.numeric(Sys.time()), origin = "1970-01-01", tz = tz)
 }
 
+#' Compute Local Time from Coordinated Universal Time (UTC/GMT).
+#'
+#' @param date Character. Date "yyyy-mm-dd".
+#' @param time Character. Local time "hh:mm" or "hh:mm:ss".
+#' @param tz Character. Local time zone. See OlsonNames() or use Sys.timezone().
+#' @export
+
+localTime <- function(date = "2021-1-1", time = "12:00", tz = Sys.timezone()) {
+  local.date <- as.Date(date, optional = TRUE)
+  if (is.na(local.date)) {
+    stop('Invalid date or format "yyyy-mm-dd".', call. = FALSE)
+  }
+  x <- dateTime(local.date, time)
+  as.POSIXlt(as.numeric(x), origin = "1970-01-01", tz = tz)
+}
+
 #' Compute Date and Time of Latest Available Log.
 #'
 #' GMT and Local Posting Times.
