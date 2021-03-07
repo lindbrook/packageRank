07e0081e6e74b3f9c5a784329e08f8e6711a27ce
lindbrook <lindbrook@gmail.com>
2021-03-07 09:04:17 -0800

    amend monthlyLog()'s packageLog() syntax

--- R/packageVersionPercent.R
+++ R/packageVersionPercent.R
@@ -52,7 +52,7 @@ monthlyLog <- function(yr.mo = "2020-07") {
   start.date <- fixDate_2012(start.date)
   end.date <- fixDate_2012(end.date)
   days <- seq(start.date, end.date, by = "day")
-  lapply(days, function(d) packageLog(d, memoization = FALSE))
+  lapply(days, function(d) packageLog(date = d, memoization = FALSE))
 }
 
 computeVersionPercents <- function(lst, pkgs, cores) {
