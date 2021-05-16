6fe3290991f57e2c759d2721ffc298e5c139de4a
lindbrook <lindbrook@gmail.com>
2021-05-16 10:09:02 -0700

    add candidate lastDayMonth()

--- R/cranDownloads.R
+++ R/cranDownloads.R
@@ -824,3 +824,33 @@ singlePlot <- function(x, statistic, graphics, days.observed, points, smooth,
     }
   }
 }
+
+lastDayMonth <- function(dates) {
+  max.obs.date <- max(dates)
+  max.mo <- as.numeric(format(max.obs.date, "%m"))
+  max.yr <- as.numeric(format(max.obs.date, "%Y"))
+  max.yr.mo <- format(max.obs.date, "%Y-%m")
+  max.date <- as.Date(paste0(max.yr, "-", max.mo + 1, "-", 1)) - 1
+
+  obs.yr.mo <- unique(format(dates, "%Y-%m"))
+  obs.yr.mo <- obs.yr.mo[obs.yr.mo != max.yr.mo]
+
+  ldm <- lapply(obs.yr.mo, function(dt) {
+    parts <- as.numeric(unlist(strsplit(dt, "-")))
+    if (parts[2] < 12) {
+      next.mo <- parts[2] + 1
+      as.Date(paste0(parts[1], "-", next.mo, "-01")) - 1
+    } else {
+      as.Date(paste0(parts[1] + 1, "-", "01-01")) - 1
+    }
+  })
+
+  ldm <- do.call(c, ldm)
+
+  ip <- c(rep(FALSE, length(ldm)),
+          ifelse(max.obs.date != max.date, TRUE, FALSE))
+
+  data.frame(date = c(ldm, max.date), in.progress = ip)
+}
+
+
