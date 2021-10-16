848fe864dbac95d483893dcb6d400788e8293a07
lindbrook <lindbrook@gmail.com>
2021-10-16 13:56:24 -0700

    remove windopws exclusion

--- R/multiCore.R
+++ R/multiCore.R
@@ -4,28 +4,24 @@
 #' @noRd
 
 multiCore <- function(x = TRUE) {
-  if (.Platform$OS.type == "windows") {
-    cores <- 1L
-  } else {
-    if (is.logical(x) | is.numeric(x)) {
-      if (is.logical(x)) {
-        if (x == TRUE) {
-          cores <- parallel::detectCores()
-        } else {
-          cores <- 1L
-        }
-      } else if (is.numeric(x)) {
-        obs.cores <- parallel::detectCores()
-        if (x > obs.cores) {
-          msg <- 'For your system, the number of specified cores must be <= '
-          stop(paste0(msg, obs.cores, "."))
-        } else if (x <= 0) {
-          stop("You must specify at least one core.")
-        } else {
-          cores <- as.integer(x)
-        }
-      } else stop("'x' must either be logical or numeric.")
-    }
+  if (is.logical(x) | is.numeric(x)) {
+    if (is.logical(x)) {
+      if (x == TRUE) {
+        cores <- parallel::detectCores()
+      } else {
+        cores <- 1L
+      }
+    } else if (is.numeric(x)) {
+      obs.cores <- parallel::detectCores()
+      if (x > obs.cores) {
+        msg <- 'For your system, the number of specified cores must be <= '
+        stop(paste0(msg, obs.cores, "."))
+      } else if (x <= 0) {
+        stop("You must specify at least one core.")
+      } else {
+        cores <- as.integer(x)
+      }
+    } else stop("'x' must either be logical or numeric.")
   }
   cores
 }
