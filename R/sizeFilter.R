66a3b5113f61b8fb38a5c6b1592c04ea6f1215aa
lindbrook <lindbrook@gmail.com>
2021-10-19 10:58:20 -0700

    add parallel::parLapply() code to sizeFilter()

--- R/sizeFilter.R
+++ R/sizeFilter.R
@@ -4,20 +4,45 @@
 #' @param dat Object. Package log entries.
 #' @param packages Character. Vector of package name(s).
 #' @param cores Integer. Number of cores for parallelization.
+#' @param dev.mode Logical. Development mode uses parallel::parLapply().
 #' @export
 
-sizeFilter <- function(dat, packages, cores) {
+sizeFilter <- function(dat, packages, cores, dev.mode = FALSE) {
   size.data <- lapply(packages, cranPackageSize)
   version.data <- lapply(packages, packageHistory)
   cores <- ifelse(length(packages) > 4, cores, 1L)
-  parallel::mclapply(seq_along(dat), function(i) {
-    sz <- size.data[[i]]
-    ver <- version.data[[i]]
-    tmp <- dat[[i]]
-    latest.ver <- ver[nrow(ver), "Version"]
-    latest.size <- sz[sz$version %in% latest.ver, ]
-    leftover <- tmp[tmp$version != latest.ver, ]
-    tmp <- tmp[tmp$version == latest.ver & tmp$size >= min(latest.size$bytes), ]
-    rbind(tmp, leftover)
-  }, mc.cores = cores)
+  win.exception <- .Platform$OS.type == "windows" & cores > 1
+
+  if (dev.mode | win.exception) {
+    cl <- parallel::makeCluster(cores)
+
+    parallel::clusterExport(cl = cl, envir = environment(),
+      varlist = c("dat", "size.data", "version.data"))
+
+    parallel::parLapply(cl, seq_along(dat), function(i) {
+      sz <- size.data[[i]]
+      ver <- version.data[[i]]
+      tmp <- dat[[i]]
+      latest.ver <- ver[nrow(ver), "Version"]
+      latest.size <- sz[sz$version %in% latest.ver, ]
+      leftover <- tmp[tmp$version != latest.ver, ]
+      tmp <- tmp[tmp$version == latest.ver & tmp$size >= min(latest.size$bytes), ]
+      rbind(tmp, leftover)
+    })
+
+    parallel::stopCluster(cl)
+
+  } else {
+    # if (.Platform$OS.type == "windows") cores <- 1L
+    parallel::mclapply(seq_along(dat), function(i) {
+      sz <- size.data[[i]]
+      ver <- version.data[[i]]
+      tmp <- dat[[i]]
+      latest.ver <- ver[nrow(ver), "Version"]
+      latest.size <- sz[sz$version %in% latest.ver, ]
+      leftover <- tmp[tmp$version != latest.ver, ]
+      tmp <- tmp[tmp$version == latest.ver & tmp$size >= min(latest.size$bytes), ]
+      rbind(tmp, leftover)
+    }, mc.cores = cores)
+  }
 }
