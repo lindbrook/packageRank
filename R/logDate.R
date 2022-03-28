a5c0db9c35de2791f0eb10ce321bfee937670079
lindbrook <lindbrook@gmail.com>
2022-03-28 10:43:59 -0700

    move use of fixDate_2012() in logDate()

--- R/logDate.R
+++ R/logDate.R
@@ -29,7 +29,6 @@ logDate <- function(date = NULL, check.url = TRUE, repository = "CRAN",
 
   if (repository == "CRAN") {
     first.log <- as.Date("2012-10-01") # first RStudio CRAN mirror log.
-    local.date <- fixDate_2012(local.date)
   } else if (repository == "MRAN") {
      first.log <- as.Date("2014-09-17") # first MRAN snapshot.
   } else stop('repository must be "CRAN" or "MRAN".', call. = FALSE)
@@ -61,7 +60,8 @@ logDate <- function(date = NULL, check.url = TRUE, repository = "CRAN",
     else stop("Date in future. Snapshot not yet available.", call. = FALSE)
   }
 
-  log.date
+  if (fix.date) fixDate_2012(log.date)
+  else log.date
 }
 
 available_log <- function(local.date, tz, upload.time, warning.msg) {
