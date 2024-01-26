39612b3f04777468c9bf9806c29e94fee1238632
lindbrook <lindbrook@gmail.com>
2024-01-26 14:18:02 -0800

    add packageRank-package document stub

---
+++ R/packageRank-package.R
@@ -0,0 +1,10 @@
+# #' @keywords internal
+#' packageRank
+#'
+#' Computation and Visualization of Package Download Counts and Percentiles.
+#'
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
