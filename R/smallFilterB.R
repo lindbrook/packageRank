82acf5cfb3a690d602ef56d0859730d1b049d9bf
lindbrook <lindbrook@gmail.com>
2024-03-19 08:49:24 -0700

    format smallFilterB()

--- R/smallFilterB.R
+++ R/smallFilterB.R
@@ -4,6 +4,4 @@
 #' @param threshold Numeric. Bytes.
 #' @noRd
 
-smallFilterB <- function(dat, threshold = 1000L) {
-  dat[dat$size >= threshold, ]
-}
+smallFilterB <- function(dat, threshold = 1000L) dat[dat$size >= threshold, ]
