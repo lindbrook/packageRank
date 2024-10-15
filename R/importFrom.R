0bb021f53e105c32884e9f87378fcc6b58de8329
lindbrook <lindbrook@gmail.com>
2024-10-15 11:22:03 -0700

    amend importFrom.R

--- R/importFrom.R
+++ R/importFrom.R
@@ -1,5 +1,2 @@
 #' @importFrom ggplot2 aes element_blank element_text facet_wrap geom_hline geom_label geom_line geom_point geom_segment geom_smooth ggplot geom_vline ggtitle scale_color_manual scale_linetype_manual scale_shape_manual scale_x_continuous scale_x_log10 scale_y_continuous  scale_y_log10 theme theme_bw vars xlab ylab
-
-#' @importFrom graphics abline axis barplot dotchart legend lines mtext par points segments text title
-
 #' @importFrom rlang .data
