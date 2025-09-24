34860eea3298472a6919f9dcb0bd4e77604ff6b9
lindbrook <lindbrook@gmail.com>
2025-09-24 15:33:01 -0700

    amend/update importFrom.R

--- R/importFrom.R
+++ R/importFrom.R
@@ -1,5 +1,5 @@
-#' @importFrom ggplot2 aes element_blank element_text facet_wrap geom_hline geom_label geom_line geom_point geom_segment geom_smooth ggplot geom_vline ggtitle scale_color_manual scale_linetype_manual scale_shape_manual scale_x_continuous scale_x_log10 scale_y_continuous  scale_y_log10 theme theme_bw vars xlab ylab
+#' @importFrom graphics abline axis barplot dotchart legend lines mtext par points segments text title
+#' @importFrom ggplot2 aes annotate coord_cartesian dup_axis element_blank element_text facet_wrap geom_hline geom_label geom_line geom_point geom_raster geom_segment geom_smooth geom_tile geom_vline ggplot margin rel scale_color_manual scale_fill_distiller scale_linetype_manual scale_shape_manual scale_x_continuous scale_x_date scale_x_log10 scale_y_continuous scale_y_discrete scale_y_log10 scale_y_reverse theme theme_bw vars
 #' @importFrom rlang .data
 #' @importFrom fasttime fastPOSIXct
 #' @importFrom patchwork plot_layout plot_annotation wrap_plots
-#' @importFrom ggplot2 annotate coord_cartesian geom_raster geom_tile margin rel scale_fill_distiller scale_y_discrete scale_y_reverse
