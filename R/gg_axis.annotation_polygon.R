22456da450e895123aa119d771d1512a1eb9dc35
lindbrook <lindbrook@gmail.com>
2025-10-03 09:53:01 -0700

    amend r_v and move null.set

--- R/gg_axis.annotation_polygon.R
+++ R/gg_axis.annotation_polygon.R
@@ -4,6 +4,7 @@
 gg_axis.annotation_polygon <- function(dat, p, log.y, chatgpt, r.version,
   chatgpt.release) {
 
+  null.set <- expression(symbol("\306"))
   date.range <- range(dat$date)
   exp.dates <- seq.Date(from = date.range[1], to = date.range[2])
   obs.chatgpt <- chatgpt.release %in% exp.dates
@@ -15,7 +16,7 @@ gg_axis.annotation_polygon <- function(dat, p, log.y, chatgpt, r.version,
     r_v <- rvers.data$version
     sel <- r_date %in% exp.dates
     obs.r <- any(sel)
-    r_v <- paste("R", r_v[sel])
+    r_v <- if (obs.r) paste("R", r_v[sel])
     r_date <- r_date[sel]
   } else if (isFALSE(r.version) | isFALSE(r.version == "line")) {
     r_v <- NULL
@@ -27,8 +28,6 @@ gg_axis.annotation_polygon <- function(dat, p, log.y, chatgpt, r.version,
     # https://stackoverflow.com/questions/70370189/annotating-a-rectangle-in-r-with-ggplot2-for-a-graph-in-log-scale
     ymin <- ifelse(log.y, 0, -Inf)
     p <- gg_missingDatesPolygons(p, ymin)
-
-    null.set <- expression(symbol("\306"))
 
     if ((isTRUE(chatgpt) | isTRUE(chatgpt == "line")) &
         (isTRUE(r.version) | isTRUE(r.version == "line"))) {
